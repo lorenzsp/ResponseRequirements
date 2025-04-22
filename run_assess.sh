56a3a0f0d5fc0b76dcabb84c5c2eda94b5fd2dcb
Lorenzo <lorenzo.speri@gmail.com>
2025-04-22 10:45:33 +0200

    add freq spectrum deviation

--- run_assess.sh
+++ run_assess.sh
@@ -1,11 +1,8 @@
 # run with: nohup bash run_assess.sh > out.log &
 
 # Define GB frequencies and corresponding GPU IDs
-declare -a gb_frequencies=("5e-4")
-# "2e-4" "5e-4" "1e-3" "2e-3" "5e-3" "1e-2")
-declare -a gb_frequencies=("1e-3" "2e-3" "5e-3" "1e-2")
-declare -a gpus=("4" "4" "4" "4")
-# "1" "1" "1" "1" "1" "1")
+declare -a gb_frequencies=("1e-4" "2e-4" "5e-4" "1e-3" "2e-3" "5e-3" "1e-2")
+declare -a gpus=("1" "1" "1" "1" "1" "1" "1")
 
 # Loop through frequencies and GPUs
 for i in "${!gb_frequencies[@]}"; do
